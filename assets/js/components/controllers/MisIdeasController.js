20ae9c7ff48119d3236d9fb97d6b23839ad0957e
jazz1228 <juan.zapata19@udea.edu.co>
2018-03-20 15:55:40 -0500

    Empezamos mal2

---
+++ assets/js/components/controllers/MisIdeasController.js
@@ -0,0 +1,19 @@
+ipisis.controller('MisIdeasController', ["$scope", "IdeaService", function($scope, IdeaService) {
+    IdeaService.obtenerMisIdeas()
+      .success(function(data) {
+        $scope.ideas = data;
+      });
+  
+    $scope.mostrar = function(idea) {
+      $scope.id = idea.id;
+      $scope.titulo = idea.titulo;
+      $scope.descripcion = idea.descripcion;
+      $scope.numMiembros = idea.numMiembros;
+      $scope.numEquipos = idea.numEquipos;
+      $scope.proponentes = idea.proponentes;
+      $scope.asignaturas = idea.asignaturas;
+      $scope.prerrequisitos = idea.prerrequisitos;
+    };
+         
+      
+  }]);
